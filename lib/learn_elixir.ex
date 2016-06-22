6ceebb752d5805ac5717a4a1071ba27971607693
Kevin Sullivan <kevinmsullivan@protonmail.com>
2016-06-22 16:19:33 -0700

    Exercises from 08-comprehensions

--- lib/learn_elixir.ex
+++ lib/learn_elixir.ex
@@ -1,2 +1,25 @@
 defmodule LearnElixir do
+  @moduledoc """
+  Functions written to satisfy the assignments from www.LearnElixir.tv,
+  an excellent resource to learn all about Elixir.
+  """
+
+  @doc """
+  https://www.learnelixir.tv/episodes/08-comprehensions
+  Create a function, using for, which will return all the even
+  numbers up to a given number.
+  """
+  def even_numbers(max_num) do
+    require Integer
+    for num <- 1..max_num, Integer.is_even(num), do: num
+  end
+
+  @doc """
+  https://www.learnelixir.tv/episodes/08-comprehensions
+  Write a function, using for, which joins a list of binaries
+  together with a separator.
+  """
+  def join_list(list, separator) do
+    for binary <- list, into: "", do: "#{binary}#{separator}"
+  end
 end
--- test/learn_elixir_test.exs
+++ test/learn_elixir_test.exs
@@ -5,4 +5,13 @@ defmodule LearnElixirTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "even_numbers" do
+    assert LearnElixir.even_numbers(15) == [2, 4, 6, 8, 10, 12, 14]
+  end
+
+# is this cheating?
+  test "join_list" do
+    assert LearnElixir.join_list(["a", "b", "c", "d", "e"], "-") == "a-b-c-d-e-"
+  end
 end
